d6dfbc381af0dae54de0b534a7cb5412c42d8c95
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2022-09-10 21:16:43 -0400

    Update api-gateway-authorizer-example.py

--- lambda/api-gateway-authorizer-example.py
+++ lambda/api-gateway-authorizer-example.py
@@ -2,11 +2,8 @@ import json
 
 def lambda_handler(event, context):
     effect = None
-    headers = event['headers']
-    
-    # this is where you would validate the auth token, for example,
-    # from a DynamoDB table
-    if headers['Authorization'] == 'secureAuthToken':
+
+    if event['Authorization'] == 'secureAuthToken':
         effect = 'Allow'
     else:
         effect = 'Deny'
@@ -19,8 +16,9 @@ def lambda_handler(event, context):
                 {
                     "Action": "execute-api:Invoke",
                     "Effect": effect,
-                    # don't forget to substitute the insert the accountId, apiId, and resource
-                    "Resource": "arn:aws:execute-api:us-east-1:{accountId}:{apiId}/*/GET/{resource}"
+                    "Resource": [
+                        "arn:aws:execute-api:us-east-1:089263644322:wjauokhh64/*/GET/blog"
+                    ]
                 }
             ]
         }
